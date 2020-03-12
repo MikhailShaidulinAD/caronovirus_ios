d6d7afd4c9580f6d11b0ea64dd5861802de884da
MikhailA&I <mikhailshaidulin.ad@gmail.com>
2020-03-12 21:11:04 +0400

    Initial Commit

---
+++ CoronaVirus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaVirus
+//
+//  Created by ShaydulinsIOS on 12.03.2020.
+//  Copyright © 2020 ShaydulinsIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
