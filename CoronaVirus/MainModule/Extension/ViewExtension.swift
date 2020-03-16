16b38a513a1a4e3b97d93a971223d05ae457730b
MikhailA&I <mikhailshaidulin.ad@gmail.com>
2020-03-17 00:57:32 +0400

    add bottom navigation add screens

--- CoronaVirus/MainModule/Extension/ViewExtension.swift
+++ CoronaVirus/MainModule/Extension/ViewExtension.swift
@@ -24,3 +24,24 @@ extension UIView {
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.21).cgColor
     }
 }
+
+extension UIImage {
+    func imageWithColor(color1: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        color1.setFill()
+
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        context?.setBlendMode(CGBlendMode.normal)
+
+        let rect = CGRect(origin: .zero, size: CGSize(width: self.size.width, height: self.size.height))
+        context?.clip(to: rect, mask: self.cgImage!)
+        context?.fill(rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
+    }
+}
---
+++ CoronaVirus/NavController/NavigationBottomMenu.swift
@@ -0,0 +1,55 @@
+//
+//  NavigationBottomMenu.swift
+//  CoronaVirus
+//
+//  Created by ShaydulinsIOS on 16.03.2020.
+//  Copyright © 2020 ShaydulinsIOS. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBottomMenu: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+        delegate = self
+    }
+    
+    private func setupTabBar(){
+        tabBar.barTintColor = UIColor(red: 250/255, green: 248/255, blue: 243/255, alpha: 1)
+        let mapController = UINavigationController(rootViewController: ViewController())
+        mapController.tabBarItem.image = UIImage(named: "bar_map")
+        mapController.tabBarItem.selectedImage = UIImage(named: "bar_map")?.imageWithColor(color1: UIColor(red: 52/255, green: 120/255, blue: 246/255, alpha: 1))
+        mapController.tabBarItem.title = "Map"
+        let newsController = UINavigationController(rootViewController: NewsViewController())
+        newsController.tabBarItem.image = UIImage(named: "bar_news")
+        newsController.tabBarItem.selectedImage = UIImage(named: "bar_news")?.imageWithColor(color1: UIColor(red: 52/255, green: 120/255, blue: 246/255, alpha: 1))
+        newsController.tabBarItem.title = "News"
+        let faqController = UINavigationController(rootViewController: FaqViewController())
+        faqController.tabBarItem.image = UIImage(named: "bar_quest")
+        faqController.tabBarItem.selectedImage = UIImage(named: "bar_quest")?.imageWithColor(color1: UIColor(red: 52/255, green: 120/255, blue: 246/255, alpha: 1))
+        faqController.tabBarItem.title = "Info"
+        let profileController = UINavigationController(rootViewController: ProfileViewController())
+        profileController.tabBarItem.image = UIImage(named: "bar_profile")
+        profileController.tabBarItem.selectedImage = UIImage(named: "bar_profile")?.imageWithColor(color1: UIColor(red: 52/255, green: 120/255, blue: 246/255, alpha: 1))
+        profileController.tabBarItem.title = "Profile"
+        
+        viewControllers = [mapController, faqController, newsController, profileController]
+    }
+}
+
+extension NavigationBottomMenu: UITabBarControllerDelegate  {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+
+        guard let fromView = selectedViewController?.view, let toView = viewController.view else {
+          return false // Make sure you want this as false
+        }
+
+        if fromView != toView {
+          UIView.transition(from: fromView, to: toView, duration: 0.3, options: [.transitionFlipFromLeft], completion: nil)
+        }
+
+        return true
+    }
+}
